1044c71964c8cadf64ad78a1c15542baca0a806f
srb <rathinaboopathi.srb@gmail.com>
2025-03-29 21:29:22 +0530

    removed error handler and everything unnecessary

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-// index.js
-const RouteAnalytics = require("./lib/RouteAnalytics");
-module.exports = RouteAnalytics;
+const mongoose = require("mongoose");
+const IORedis = require("ioredis");
+const { Queue } = require("bullmq");
+const startWorker = require("./worker");
+const middleware = require("./lib/middleware");
+const traxxModel = require("./lib/model");
+
+
+async function init(mongoUri, redisUri) {
+  await mongoose.connect(mongoUri);
+  const redis = new IORedis(redisUri);
+  new Queue("route-analytics-queue", { connection: redis });
+  startWorker(mongoUri, redisUri)
+  return {status:""};
+}
+
+module.exports = {
+  init,
+  middleware,
+  traxxModel
+};
